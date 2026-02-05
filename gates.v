319286cc67cf22719b7c91e7c4d47209fca7d7c7
subb-2 <zhsks312@gmail.com>
2026-02-05 12:05:25 +0900

    project_0122

---
+++ gates.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/01/22 09:15:04
+// Design Name: 
+// Module Name: gates
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module gates(
+
+    );
+endmodule

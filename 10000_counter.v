e5e9ed1f8a27203d962896a0ab9a6d64e57a7937
subb-2 <zhsks312@gmail.com>
2026-02-05 12:06:04 +0900

    10000_counter

---
+++ 10000_counter.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2026/01/26 15:53:24
+// Design Name: 
+// Module Name: 10000_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module 10000_counter(
+
+    );
+endmodule
